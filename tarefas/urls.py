3b17571e497f17ffb5a0e6214a68b66a364de92c
Guilherme Moreno <guilhermemoreno200@gmail.com>
2026-02-20 18:36:18 -0300

    alterações feitas

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [ 
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('adicionar', views.adicionar, name='adicionar'),
+    path('tarefa/<int:id>', views.editar, name='editar'),
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import TarefaForm
+from .models import Tarefa
 def home(request):
-    return render(request, 'tarefas/home.html')
+    lista = Tarefa.objects.all()
+    return render(request, 'tarefas/home.html', {'tarefas': lista})
+
+def adicionar(request): 
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else: 
+        form = TarefaForm()
+    return render(request, 'tarefas/add.html', {'form': form})
